0949a05f05ce8dd589896aa949c8d0f3e162c5cb
Mike Bird <mike@arrendy.com>
2022-01-19 17:19:08 -0500

    basic functioning example

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # edge-sdk-javascript-example
 A step by step guide for getting your Javascript programs to communicate with InOrbit platform on behalf of robots 
+
+Requirements
+- Node version 16.13.0 or newer 
+
+
+Steps
+- Clone this repo
+- Get your API key and set it as an environment variable
+- Update your robotID (line 15) to a unique identifier
---
+++ index.js
@@ -0,0 +1,60 @@
+/**
+ * InOrbit Edge SDK Example showing how to send data belonging to one robot
+ * to the InOrbit Platform.
+ *
+ * Copyright 2021 InOrbit, Inc.
+ */
+
+ import { InOrbit } from '@inorbit/edge-sdk';
+
+ function sleep(ms) {
+   return new Promise((resolve) => setTimeout(resolve, ms));
+ }
+ 
+ async function main() {
+   const robotId = '<Update this value>';
+ 
+   // Initialize the SDK reading the InOrbit App Key from the environment
+   const sdk = new InOrbit({
+     appKey: process.env.INORBIT_APP_KEY,
+     // Include logging to the console
+     logger: {
+       info: console.log,
+       error: console.log,
+       warn: console.log,
+     }
+   });
+ 
+   // Initialize the robot connection
+   await sdk.connectRobot({ robotId, name: 'Inorbito' });
+ 
+   while (true) {
+     // Publish Key-Values for battery and status
+     await sdk.publishCustomDataKV(robotId, {
+       battery: Math.random() * 100,
+       status: Math.random() > 0.5 ? 'Mission' : 'Idle'
+     });
+ 
+     // Publish a random pose
+     await sdk.publishPose(robotId, {
+       ts: new Date().getTime(),
+       x: Math.random() * 20 + 20,
+       y: Math.random() * 20 + 10,
+       yaw: Math.random() * Math.PI * 2,
+       frameId: 'map'
+     });
+ 
+     // Publish random speed
+     await sdk.publishOdometry(robotId, {
+       tsStart: new Date().getTime(),
+       ts: new Date().getTime(),
+       speed: {
+         linear: Math.random() * 10,
+         angular: Math.random() * Math.PI
+       }
+     });
+     await sleep(1000);
+   }
+ }
+ 
+ main();
